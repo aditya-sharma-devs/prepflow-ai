5372a5465a9041c0fdb9044a435f735225ccc767
Aditya Sharma <adityasharma08oct@gmail.com>
2026-06-03 13:18:55 +0530

    Fixed URL bug

--- client/src/components/dashboard/DashboardLayout.jsx
+++ client/src/components/dashboard/DashboardLayout.jsx
@@ -1,31 +1,75 @@
-import DashboardSidebar from "../DashboardSidebar";
-import { Outlet } from "react-router-dom";
+// import DashboardSidebar from "../DashboardSidebar";
+// import { Outlet } from "react-router-dom";
+// import { useEffect } from "react";
+// import axios from "axios";
+
+
+// function DashboardLayout() {
+//   useEffect(() => {
+//     async function applyTheme() {
+//       try {
+//         const token = localStorage.getItem("token");
+
+//         const res = await axios.get("http://localhost:5000/api/settings", {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
+
+//         const darkMode = res.data.settings?.darkMode ?? false;
+
+//         document.body.classList.toggle("dark-mode", darkMode);
+//       } catch(error) {
+//         console.log(error)
+//         console.log("Theme load failed");
+//       }
+//     }
+
+//     applyTheme();
+//   }, []);
+
+//   return (
+//     <div className="dashboard-layout">
+//       <DashboardSidebar />
+
+//       <section className="dashboard-main">
+//         <Outlet />
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default DashboardLayout;
+
+
 import { useEffect } from "react";
 import axios from "axios";
-
+import DashboardSidebar from "../DashboardSidebar";
+import { Outlet } from "react-router-dom";
+import { API_URL } from "../../config";
 
 function DashboardLayout() {
   useEffect(() => {
-    async function applyTheme() {
+    async function applySavedTheme() {
       try {
         const token = localStorage.getItem("token");
 
-        const res = await axios.get("http://localhost:5000/api/settings", {
+        const res = await axios.get(`${API_URL}/api/settings`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
 
-        const darkMode = res.data.settings?.darkMode ?? false;
+        const savedDarkMode = res.data.settings?.darkMode ?? false;
 
-        document.body.classList.toggle("dark-mode", darkMode);
-      } catch(error) {
+        document.body.classList.toggle("dark-mode", savedDarkMode);
+      } catch (error) {
         console.log(error)
-        console.log("Theme load failed");
+        console.log("Failed to apply saved theme");
       }
     }
 
-    applyTheme();
+    applySavedTheme();
   }, []);
 
   return (
